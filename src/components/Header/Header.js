f633ff29d1e9c626c68ef083947226c4192b99b4
Kchar <38297595+kevinlichang@users.noreplay.github.com>
2018-12-06 18:32:44 -0500

    Header draft

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Header.css";
 
+const Header = () => (
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <img src="./Images/ewsLogo.png" alt="Explore With Sko" />
+      <p class="lead">films by Stephen Ko</p>
 
+      
+    </div>
+  </div>
+
+)
 
 export default Header;
