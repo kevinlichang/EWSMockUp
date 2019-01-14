c20e3865ae727c95af2b16c24fb2c6fab59a0478
Kchar <38297595+kevinlichang@users.noreply.github.com>
2019-01-13 23:28:48 -0500

    set up whoisSko and video page placeholder

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import About from './components/About/About';
 import WhoIsSko from './components/WhoIsSko/WhoIsSko';
 import Home from './components/Home/Home';
+import VideoPage from './components/VideoPage/Video';
 
 
 class App extends Component {
---
+++ src/components/VideoPage/Video.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class VideoPage extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          Video Page
+
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default VideoPage;
