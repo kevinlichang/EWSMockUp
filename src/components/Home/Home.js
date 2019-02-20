2a5ec0bfaf95b370188dcbb25551f164d04007a7
Kchar <38297595+kevinlichang@users.noreply.github.com>
2019-02-19 19:22:12 -0500

    edit in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
     return (
       <div className="container-fluid">
         <div className="text-center">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/R_941LBourA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/R_941LBourA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="myanmar-vid"></iframe>
         </div>
       </div>
     )
